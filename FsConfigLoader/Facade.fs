cc1d04c5eb6425d8211f89876c7512737e20919f
Anthony Harris <anthony@ah.id.au>
2024-07-12 16:08:44 +0100

    Facilitate consistent use of the Config Loader library for a simple scenario.

---
+++ FsConfigLoader/Facade.fs
@@ -0,0 +1,19 @@
+﻿module FsConfigLoader.Facade
+
+let tryRun readConfigResult runWithProgramSpecificConfiguration argv =
+    Core.ConfigSource(Array.tryItem 1 argv, Array.item 0 argv)
+    |> FromJson.loadFromFile
+    |> Result.bind readConfigResult
+    |> Result.map runWithProgramSpecificConfiguration
+
+module SimpleConsoleApp =
+    let tryWithConfiguration readConfigResult runWithProgramSpecificConfiguration argv =
+        System.Console.OutputEncoding <- System.Text.Encoding.UTF8
+
+        tryRun readConfigResult runWithProgramSpecificConfiguration argv
+        |> function
+            | Result.Ok _ -> 0
+            | Result.Error msg ->
+                // ReSharper disable once FSharpInterpolatedString
+                printfn "[\u274c] {%s}" msg
+                1
